1eee079362eab82698c13474a98d3fb7b587ea9b
wesleynjerenga <wesleynjerenga9@gmail.com>
2025-04-16 09:11:49 +0300

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./App.css";
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 function App() {
   const [tasks, setTasks] = useState([]);
   const [task, setTask] = useState("");
